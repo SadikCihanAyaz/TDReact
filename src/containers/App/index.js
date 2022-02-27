8597117f1f77b519f28b7e22f81a26cb87d0e4de
Sadık Cihan Ayaz <scayaz.19.19@gmail.com>
2022-02-27 17:40:01 +0300

    feat: project structure is changed

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-    Hello world!
+    learn react
   </h1>
   );
 }
