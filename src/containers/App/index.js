db7b9827e9f68bf048b8c78b9d1a97cdacd02ee0
Sadık Cihan Ayaz <scayaz.19.19@gmail.com>
2022-03-01 14:03:59 +0300

    feat: little changes is applying to index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    learn react
-  </h1>
+    <div class="container mx-auto bg-indigo-500">
+<form>
+    <h1>Everything is work properly</h1>
+</form>
+</div>
   );
 }
 
